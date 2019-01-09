3b3c545cb583c132706ec02fde0ee9a7ac557d35
nehrman <nicolas.ehrman@gmail.com>
2019-01-09 09:39:48 +0100

    Adding more functions

--- main_deployer.tf
+++ main_deployer.tf
@@ -1,12 +1,14 @@
-######
-# Azure Configuration 
-######
+#######################
+# Azure Configuration #
+#######################
 
 provider "azurerm" {}
 
+# Create Resource Group
+
 resource "azurerm_resource_group" "arm_rg" {
   name     = "test-neh"
-  location = "francecentral"
+  location = "${lookup(var.cloud_region, var.cloud_provider,)}"
 
   tags {
     environnement = "test"
@@ -17,12 +19,14 @@ resource "azurerm_resource_group" "arm_rg" {
 
   count = "${var.cloud_provider == "arm" ? 1 : 0}"
 }
+
+# Create Virtual Network
 
 resource "azurerm_virtual_network" "arm_vnet" {
   name                = "test-vnet"
   location            = "${azurerm_resource_group.arm_rg.location}"
   resource_group_name = "${azurerm_resource_group.arm_rg.name}"
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["${var.global_address_space}"]
 
   tags {
     environnement = "test"
@@ -34,13 +38,17 @@ resource "azurerm_virtual_network" "arm_vnet" {
   count = "${var.cloud_provider == "arm" ? 1 : 0}"
 }
 
+# Create Subnets attached to Virtual Network
+
 resource "azurerm_subnet" "arm_subnet" {
   name                 = "test-subnet"
   virtual_network_name = "${azurerm_virtual_network.arm_vnet.name}"
   resource_group_name  = "${azurerm_resource_group.arm_rg.name}"
-  address_prefix       = "10.0.1.0/24"
+  address_prefix       = "${cidrsubnet(var.global_address_space, 8, 1)}"
   count                = "${var.cloud_provider == "arm" ? 1 : 0}"
 }
+
+# Create Storage Account
 
 resource "azurerm_storage_account" "arm_storageaccount" {
   name                     = "testnehstoacct"
@@ -59,7 +67,31 @@ resource "azurerm_storage_account" "arm_storageaccount" {
   count = "${var.cloud_provider == "arm" ? 1 : 0}"
 }
 
-# Create Network Nic to use with VM
+resource "azurerm_dns_zone" "arm_dns_zone" {
+  name                = "demo${var.cloud_provider}.my-v-world.com"
+  resource_group_name = "${azurerm_resource_group.arm_rg.name}"
+  zone_type           = "Public"
+
+  count = "${var.cloud_provider == "arm" ? 1 : 0}"
+}
+
+resource "azurerm_public_ip" "arm_vm_pub_ip" {
+  name                         = "testpubip-${count.index}"
+  location                     = "${azurerm_resource_group.arm_rg.location}"
+  resource_group_name          = "${azurerm_resource_group.arm_rg.name}"
+  public_ip_address_allocation = "static"
+
+  tags {
+    environnement = "test"
+    owner         = "neh"
+    purpose       = "Testing"
+    cloud         = "${var.cloud_provider}"
+  }
+
+  count = "${var.vm_count * (var.cloud_provider == "arm" ? 1 : 0)}"
+}
+
+# Create Network Nics to use with VM
 resource "azurerm_network_interface" "arm_nics" {
   count                     = "${var.vm_count * (var.cloud_provider == "arm" ? 1 : 0)}"
   name                      = "nic-${count.index}"
@@ -97,22 +129,25 @@ resource "azurerm_network_security_group" "arm_nsg" {
   count = "${var.cloud_provider == "arm" ? 1 : 0}"
 }
 
+# Create Custom Rules for Security Group
+
 resource "azurerm_network_security_rule" "arm_custom_rules" {
-  count                       = "${length(var.custom_security_rules)}"
+  count                       = "${length(var.arm_custom_security_rules) * (var.cloud_provider == "arm" ? 1 : 0)}"
   resource_group_name         = "${azurerm_resource_group.arm_rg.name}"
   network_security_group_name = "${azurerm_network_security_group.arm_nsg.name}"
-  name                        = "${lookup(var.custom_security_rules[count.index], "name")}"
-  priority                    = "${lookup(var.custom_security_rules[count.index], "priority")}"
-  direction                   = "${lookup(var.custom_security_rules[count.index], "direction")}"
-  access                      = "${lookup(var.custom_security_rules[count.index], "access")}"
-  protocol                    = "${lookup(var.custom_security_rules[count.index], "protocol")}"
-  source_port_range           = "${lookup(var.custom_security_rules[count.index], "source_port_range")}"
-  destination_port_range      = "${lookup(var.custom_security_rules[count.index], "destination_port_range")}"
-  source_address_prefix       = "${lookup(var.custom_security_rules[count.index], "source_address_prefix")}"
-  destination_address_prefix  = "${lookup(var.custom_security_rules[count.index], "destination_address_prefix")}"
+  name                        = "${lookup(var.arm_custom_security_rules[count.index], "name")}"
+  priority                    = "${lookup(var.arm_custom_security_rules[count.index], "priority")}"
+  direction                   = "${lookup(var.arm_custom_security_rules[count.index], "direction")}"
+  access                      = "${lookup(var.arm_custom_security_rules[count.index], "access")}"
+  protocol                    = "${lookup(var.arm_custom_security_rules[count.index], "protocol")}"
+  source_port_range           = "${lookup(var.arm_custom_security_rules[count.index], "source_port_range")}"
+  destination_port_range      = "${lookup(var.arm_custom_security_rules[count.index], "destination_port_range")}"
+  source_address_prefix       = "${lookup(var.arm_custom_security_rules[count.index], "source_address_prefix")}"
+  destination_address_prefix  = "${lookup(var.arm_custom_security_rules[count.index], "destination_address_prefix")}"
 }
 
-# Create Azure Instances
+# Create Azure VMs
+
 resource "azurerm_virtual_machine" "arm_vm" {
   count                 = "${var.vm_count * (var.cloud_provider == "arm" ? 1 : 0)}"
   name                  = "vm-${count.index}"
@@ -159,10 +194,189 @@ resource "azurerm_virtual_machine" "arm_vm" {
   }
 }
 
+resource "azurerm_dns_a_record" "arm_vm_record" {
+  name                = "${element(azurerm_virtual_machine.arm_vm.*.name, count.index)}"
+  zone_name           = "${azurerm_dns_zone.arm_dns_zone.name}"
+  resource_group_name = "${azurerm_resource_group.arm_rg.name}"
+  ttl                 = "300"
+  records             = ["${element(azurerm_public_ip.arm_vm_pub_ip.*.ip_address, count.index)}"]
+}
 
-######
-# AWS Configuration 
-######
+#####################
+# AWS Configuration #
+#####################
 
-provider "aws" {}
+provider "aws" {
+  region = "${lookup(var.cloud_region, "aws")}"
+}
 
+resource "aws_vpc" "ec2_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags {
+    environnement = "test"
+    owner         = "neh"
+    purpose       = "Testing"
+    cloud         = "${var.cloud_provider}"
+  }
+
+  count = "${var.cloud_provider == "aws" ? 1 : 0}"
+}
+
+resource "aws_subnet" "ec2_subnet" {
+  vpc_id                  = "${aws_vpc.ec2_vpc.id}"
+  cidr_block              = "${cidrsubnet(var.global_address_space, 8, 1)}"
+  map_public_ip_on_launch = true
+
+  tags {
+    environnement = "test"
+    owner         = "neh"
+    purpose       = "Testing"
+    cloud         = "${var.cloud_provider}"
+  }
+
+  count = "${var.cloud_provider == "aws" ? 1 : 0}"
+}
+
+resource "aws_internet_gateway" "ec2_igw" {
+  vpc_id = "${aws_vpc.ec2_vpc.id}"
+
+  tags {
+    environnement = "test"
+    owner         = "neh"
+    purpose       = "Testing"
+    cloud         = "${var.cloud_provider}"
+  }
+
+  count = "${var.cloud_provider == "aws" ? 1 : 0}"
+}
+
+resource "aws_route_table" "ec2_rtb" {
+  vpc_id = "${aws_vpc.ec2_vpc.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.ec2_igw.id}"
+  }
+
+  tags {
+    environnement = "test"
+    owner         = "neh"
+    purpose       = "Testing"
+    cloud         = "${var.cloud_provider}"
+  }
+
+  count = "${var.cloud_provider == "aws" ? 1 : 0}"
+}
+
+resource "aws_route_table_association" "ec2_rtb_assoc" {
+  subnet_id      = "${aws_subnet.ec2_subnet.id}"
+  route_table_id = "${aws_route_table.ec2_rtb.id}"
+
+  count = "${var.cloud_provider == "aws" ? 1 : 0}"
+}
+
+resource "aws_security_group" "ec2_sg" {
+  name        = "pTFE_sg"
+  description = "Security Group allowing access to pTFE instance"
+  vpc_id      = "${aws_vpc.ec2_vpc.id}"
+
+  tags {
+    environnement = "test"
+    owner         = "neh"
+    purpose       = "Testing"
+    cloud         = "${var.cloud_provider}"
+  }
+
+  count = "${var.cloud_provider == "aws" ? 1 : 0}"
+}
+
+resource "aws_security_group_rule" "ec2_custom_rules" {
+  count             = "${length(var.ec2_custom_security_rules) * (var.cloud_provider == "aws" ? 1 : 0)}"
+  type              = "${lookup(var.ec2_custom_security_rules[count.index], "type")}"
+  from_port         = "${lookup(var.ec2_custom_security_rules[count.index], "from_port")}"
+  to_port           = "${lookup(var.ec2_custom_security_rules[count.index], "to_port")}"
+  protocol          = "${lookup(var.ec2_custom_security_rules[count.index], "protocol")}"
+  cidr_blocks       = "${var.ec2_cidr_blocks}"
+  description       = "${lookup(var.ec2_custom_security_rules[count.index], "description")}"
+  security_group_id = "${aws_security_group.ec2_sg.id}"
+}
+
+resource "aws_instance" "ec2_vm" {
+  count                       = "${var.vm_count * (var.cloud_provider == "aws" ? 1 : 0)}"
+  ami                         = "${data.aws_ami.ubuntu.id}"
+  instance_type               = "t2.large"
+  subnet_id                   = "${aws_subnet.ec2_subnet.id}"
+  private_ip                  = "${cidrhost(aws_subnet.ec2_subnet.cidr_block, count.index + 100)}"
+  associate_public_ip_address = "true"
+  vpc_security_group_ids      = ["${aws_security_group.ec2_sg.id}"]
+  key_name                    = "${var.global_key_name}"
+
+  tags {
+    environnement = "test"
+    owner         = "neh"
+    purpose       = "Testing"
+    cloud         = "${var.cloud_provider}"
+  }
+}
+
+#####################
+# GCP Configuration #
+#####################
+
+provider "google" {}
+
+resource "google_compute_network" "gcp_net" {
+  name                    = "test"
+  auto_create_subnetworks = false
+  count                   = "${var.cloud_provider == "gcp" ? 1 : 0}"
+}
+
+resource "google_compute_subnetwork" "gcp_subnets" {
+  name          = "testsubnet"
+  ip_cidr_range = "${var.global_address_space}"
+  region        = "${lookup(var.cloud_region, var.cloud_provider,)}"
+  network       = "${google_compute_network.gcp_net.self_link}"
+
+  count = "${1 * (var.cloud_provider == "gcp" ? 1 : 0)}"
+}
+
+resource "google_compute_address" "gcp_int_addr" {
+  name         = "test-int"
+  subnetwork   = "${element(google_compute_subnetwork.gcp_subnets.*.self_link, count.index)}"
+  address_type = "INTERNAL"
+
+  count = "${var.cloud_provider == "gcp" ? 1 : 0}"
+}
+
+resource "google_compute_address" "gcp_ext_addr" {
+  name = "test-ext"
+
+  count = "${var.cloud_provider == "gcp" ? 1 : 0}"
+}
+
+// resource "google_compute_firewall" "gcp_custom_rules" {}
+
+resource "google_compute_instance" "gcp_vm" {
+  name                      = "test-vm"
+  machine_type              = "n1-standard-4"
+  zone                      = "europe-west1-b"
+  allow_stopping_for_update = true
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1804-lts"
+    }
+  }
+
+  network_interface {
+    network    = "${google_compute_network.gcp_net.self_link}"
+    network_ip = "${google_compute_address.gcp_int_addr.address}"
+
+    access_config {
+      nat_ip = "${google_compute_address.gcp_ext_addr.address}"
+    }
+  }
+
+  count = "${var.vm_count * (var.cloud_provider == "gcp" ? 1 : 0)}"
+}
--- variables.tf
+++ variables.tf
@@ -1,17 +1,60 @@
-# Variable uses to define which cloud provider should be use to deploy pTFE
+# Variable used to define which cloud provider should be use to deploy pTFE
 variable "cloud_provider" {
-  default = "aws"
+  default = "arm"
+}
+
+# Variable used to define which region in regards to which cloud provider
+
+variable "cloud_region" {
+  type = "map"
+
+  default = {
+    arm = "francecentral"
+    aws = "eu-central-1"
+    gcp = "eu-west1"
+  }
+}
+
+# Variable used to define which dns zone in regards to which cloud provider
+
+variable "cloud_dns_zone" {
+  type = "map"
+
+  default = {
+    arm = "demoaz"
+    aws = "demoaws"
+    gcp = "demogcp"
+  }
 }
 
 # Variable uses to define the count of Instances to deploy
 variable "vm_count" {
-  default = "2"
+  default = "3"
+}
+
+# Global variable for SSH key name
+
+variable "global_key_name" {
+  default = "vault-test"
+}
+
+# Global Address space Variable
+
+variable "global_address_space" {
+  description = "Define the global address space used by Cloud Network"
+  default     = "10.0.0.0/16"
+}
+
+# Global CIDR Blocks list used for Security Rules configuration
+
+variable "ec2_cidr_blocks" {
+  default = ["0.0.0.0/0"]
 }
 
 # Global variables used to define configuration of security rules whatever the cloud provider is
 
-variable "custom_security_rules" {
-  description = "Create all security rules needed by default"
+variable "arm_custom_security_rules" {
+  description = "Create all security rules needed by default for Azure"
   type        = "list"
 
   default = [
@@ -37,7 +80,7 @@ variable "custom_security_rules" {
       source_address_prefix      = "*"
       destination_address_prefix = "*"
     },
-        {
+    {
       name                       = "SSH"
       priority                   = "1002"
       direction                  = "Inbound"
@@ -48,7 +91,7 @@ variable "custom_security_rules" {
       source_address_prefix      = "*"
       destination_address_prefix = "*"
     },
-            {
+    {
       name                       = "REPLICATED"
       priority                   = "1003"
       direction                  = "Inbound"
@@ -58,6 +101,49 @@ variable "custom_security_rules" {
       destination_port_range     = "8800"
       source_address_prefix      = "*"
       destination_address_prefix = "*"
+    },
+  ]
+}
+
+variable "ec2_custom_security_rules" {
+  description = "Create all security rules needed by default for Azure"
+  type        = "list"
+
+  default = [
+    {
+      type        = "ingress"
+      from_port   = "22"
+      to_port     = "22"
+      protocol    = "tcp"
+      description = "SSH access TFE"
+    },
+    {
+      type        = "ingress"
+      from_port   = "80"
+      to_port     = "80"
+      protocol    = "tcp"
+      description = "HTTP access TFE"
+    },
+    {
+      type        = "ingress"
+      from_port   = "443"
+      to_port     = "443"
+      protocol    = "tcp"
+      description = "HTTPS access TFE"
+    },
+    {
+      type        = "ingress"
+      from_port   = "8800"
+      to_port     = "8800"
+      protocol    = "tcp"
+      description = "Access TFE"
+    },
+    {
+      type        = "egress"
+      from_port   = "0"
+      to_port     = "65535"
+      protocol    = "-1"
+      description = "Allow all"
     },
   ]
 }
